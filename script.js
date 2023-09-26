aa96f1a13146c16386afa664b010e62ef4c67b99
Gustavo Rodrigues <87579597+duguzin@users.noreply.github.com>
2023-09-26 00:41:55 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,41 @@
 const toggleMenuOpen = () => document.body.classList.toggle("open");
 
+function pesquisarPalavra() {
+    const palavraPesquisada = document.getElementById('input-postagens').value.toLowerCase();
+    const elementosTexto = document.querySelectorAll('*:not(script):not(style)');
+  
+    let palavraEncontrada = false;
+  
+    for (const elemento of elementosTexto) {
+      const conteudo = elemento.textContent.toLowerCase();
+      if (conteudo.includes(palavraPesquisada)) {
+        // Encontrou a palavra, então rola suavemente até o elemento
+        elemento.scrollIntoView({ behavior: 'smooth', block: 'center' });
+        palavraEncontrada = true;
+        break;
+      }
+    }
+  
+    if (!palavraEncontrada) {
+      // Se a palavra não foi encontrada, exibe um alerta
+      alert('Palavra não encontrada.');
+    }
+  }
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    const btnPesquisar = document.getElementById('btn-pesquisar');
+    const inputPesquisa = document.getElementById('input-postagens');
+  
+    btnPesquisar.addEventListener('click', pesquisarPalavra);
+    inputPesquisa.addEventListener('keypress', function(event) {
+      if (event.key === 'Enter') {
+        pesquisarPalavra();
+      }
+    });
+  });
+  
+  
+  
+  
+  
+  
